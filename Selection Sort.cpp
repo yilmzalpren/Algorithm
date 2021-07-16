1ba187f9fb690256bfe9a3c6482a2538c3195a59
YILMAZ ALP3R3N <74132936+yilmzalpren@users.noreply.github.com>
2021-07-16 18:32:10 +0300

    Update Selection Sort.cpp

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -1,32 +1,41 @@
 #include <iostream>
 #include <stdlib.h>
-#define SIZE 100
-int numbers[SIZE]{ 0 }; 
+#define SIZE 100 
+/* 
+	in a step, we search the smallest number and swap smallest numbers index between mother loop iterator's index.
+*/
+
+int numbers[SIZE]; 
 
 int main() {
-	int temp = 0; 
-	int minNumberIndex = -1 ; 
-	srand(time(NULL)); 
+	srand(time(NULL));
+	// rand() % [n] whatever you want
 	for (int i = 0; i < SIZE; ++i) {
-		numbers[i] = rand() % 111; 
+		numbers[i] = rand() % 97; 
 	}
-	for (int i = 0; i < SIZE - 1; ++i) {
-		minNumberIndex = i;
-		
-		for (int j = i + 1; j < SIZE; ++j) {
-			if (numbers[j] < numbers[minNumberIndex]) {
-				minNumberIndex = j; 
+	int min_index; 
+	int temp;
+
+	for (int i = 0; i < SIZE -1 ; ++i) {
+		min_index = i;
+		for (int j = i+1; j < SIZE;  ++j) {
+			if (numbers[j] < numbers[min_index]) {
+				min_index = j;
 			}
-			else; 
 		}
-		temp = numbers[i];
-		numbers[i] = numbers[minNumberIndex]; 
-		numbers[minNumberIndex] = temp; 
+		temp = numbers[min_index]; 
+		numbers[min_index] = numbers[i]; 
+		numbers[i] = temp; 
 	}
 
+	// print
+	system("cls");
 	for (int i = 0; i < SIZE; ++i) {
-		std::cout << numbers[i] << '\n'; 
+		std::cout << i+1 << '\t' << numbers[i] << '\n' ; 
 	}
+	
+
+
 
 	return 0; 
 }
